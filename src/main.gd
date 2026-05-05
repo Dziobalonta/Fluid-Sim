c6aebdccc8b621fef4875e917ab87d0e3f05b9b7
Tomek <tom.dziob@gmail.com>
2026-05-05 13:38:42 +0200

    Added basic sph implementation with smoothing function

--- src/main.gd
+++ src/main.gd
@@ -4,6 +4,9 @@ extends Node2D
 
 @export var particle_scene: PackedScene
 @export var particle_count: int = 200
+var particles_list: Array[Particle] = []
+
+var smoothing_radius: float = 60.0
 
 func _ready() -> void:
 	var screen_rect = get_viewport_rect()
@@ -30,6 +33,32 @@ func spawn_particles(bounding_box: Rect2):
 		
 		# Adding a particle to the scene	
 		add_child(p)
-		
-		
+		particles_list.append(p)
+
+func _physics_process(delta: float) -> void:
+	for p in particles_list:
+			p.density = calculate_density(p.position)
 	
+func smoothing_function(distance: float, radius: float) -> float:
+	var value = max(0, radius * radius - distance * distance)
+	return value * value * value
+	
+func calculate_density(sample_point: Vector2) -> float:
+	var density: float = 0.0
+	const mass: float = 1.0
+
+	var radius_sq = smoothing_radius * smoothing_radius
+	
+	for p in particles_list:
+		var dst_sq = p.position.distance_squared_to(sample_point)
+		
+		if dst_sq >= radius_sq:
+			continue
+		
+		
+		var value = radius_sq - dst_sq # Smoothing function
+		var influence = value * value * value
+		
+		density += mass * influence
+			
+	return density
--- src/particle.gd
+++ src/particle.gd
@@ -6,12 +6,25 @@ var radius: float = 10.0
 var damping: float = 0.6
 var boundary: Rect2
 var gravity: Vector2 = Vector2(0, 981.0)
+var density: float = 0.0
+
+@export var density_gradient: Gradient
 
 func set_boundary(new_boundary: Rect2):
 	boundary = new_boundary
 	
 func _draw() -> void:
-	draw_circle(Vector2.ZERO, radius, Color.WHITE_SMOKE, true)
+	var current_color: Color
+	if density_gradient != null:
+		var normalized_density = clamp(density / 20.0 ,0.0, 1.0)
+		
+		current_color = density_gradient.sample(normalized_density)
+		
+	else:
+		# Emergency color
+		current_color = Color.WHITE_SMOKE
+	
+	draw_circle(Vector2.ZERO, radius, current_color, true)
 
 func _ready() -> void:
 	pass
@@ -21,6 +34,7 @@ func _physics_process(delta: float) -> void:
 	position += velocity * delta
 	
 	check_boundary()
+	queue_redraw()
 	
 func check_boundary():
 	if boundary == Rect2(): return
